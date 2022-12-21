ca3bc6b83a5075a1f8280618f328c66529fd2304
Raj <srajdip920@gmail.com>
2022-12-22 01:02:22 +0600

    api Repositroy prtten

---
+++ FirstApiRepPertten/ApplicationDbCon/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FirstApiRepPertten.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstApiRepPertten.ApplicationDbCon
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<User> users { get; set; }
+    }
+}
---
+++ FirstApiRepPertten/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstApiRepPertten.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please Enter Your Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please Enter Your Address")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Please Enter Your PhoneNumber")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Please Enter Your Email")]
+
+        public string Email { get; set; }
+    }
+}
